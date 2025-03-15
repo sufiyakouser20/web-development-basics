5b58311104512935a452accf7fc2f1d736040d34
sufiyakouser20 <kousersufiya20@gmail.com>
2025-03-15 10:50:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// let person={
+//     name:"sufiya"
+//     age:18;
+// };
+// //Dot notation
+// person.name="sufiya"
+// person["age"]=18
+// console.log(person)
+// let selectedfruit=["apple,mango,kiwi"]
+// console.log(selectedfruit)
+//     selectedfruit[3]="dragonfruit"
+//     selectedfruit=(selectedfruit,length)
+// function square(number){
+//     return number*number;
+// }
+// function cube(number){
+//     return number*number*number;
+// }
+
+// console.log(square(5));
+// console.log(cube(5));
+windows.alert("your 50% data is over")
+
+
+
+
+  
